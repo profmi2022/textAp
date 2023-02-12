b89e48db7730b8adb5331d54f282d46ac8a6a54e
profmi2022 <profmi2022@gmail.com>
2023-02-13 01:33:08 +0600

    Задача на функциональное программирование курсовой "Java Core" выполнена

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+        scanner.useDelimiter("\n");
+        System.out.println("Введите текст: ");
+        String text = scanner.next();
+
+        TextWork textWork = new TextWork(text);
+
+//      TextWork textWork = new TextWork("the quick brown fox jumps over the lazy dog");
+
+        textWork.printMap();
     }
 }
